0caaa0a900badd57e4816e7505c6011279b100a3
serverbot81 <serverbot81@gmail.com>
2024-12-29 00:46:02 +0600

    Update 00TSt.js

--- scripts/commands/00TSt.js
+++ scripts/commands/00TSt.js
@@ -19,7 +19,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'pairing.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.postimg.cc/X7R3CLmb/267378493-3075346446127866-4722502659615516429-n.png", path);
+    if (!existsSync(path)) await downloadFile("https://i.postimg.cc/nVKpqMyG/20241228-185915.png", path);
 }
 
 async function makeImage({ one, two }) {
